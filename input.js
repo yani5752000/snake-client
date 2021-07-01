b4a94aa5ccb85c697acc598421df739f8b623170
Farid Asadpour <yani5752000@yahoo.com>
2021-07-01 00:19:20 +0000

    input

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,9 +12,31 @@ const setupInput = function () {
   return stdin;
 };
 
+/*
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};*/
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
